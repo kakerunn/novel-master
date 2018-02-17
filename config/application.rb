3cce109af5be9b2442265be58bab5e9b2e2c4311
kakerunn <04kakerusiina17@gmail.com>
2018-02-17 12:42:42 +0000

    gemfile

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Novel
 
 config.action_controller.action_on_unpermitted_parameters = :raise
 
+config.i18n.default_locale = :ja
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
