ed37be58cc42b72df9f14d7b7191192ef9adc02d
小田原光太 <kouta@odawaramitsumoto-no-MacBook-Air.local>
2018-02-28 12:10:57 +0900

    modify controller syntaxs

--- app/controllers/another_controller.rb
+++ app/controllers/another_controller.rb
@@ -3,5 +3,5 @@ class AnotherController < ApplicationController
   def show
     @option = Option.find(7)
   end
-  
+
 end
--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,52 +1,51 @@
 class ContributionsController < ApplicationController
+before_action :set_contribute, only: [:show, :edit, :update, :destroy]
 
-    def index
-        @contributions = Contribution.all.order(created_at: 'desc' )
+  def index
+    @contributions = Contribution.all.order(created_at: 'desc' )
+  end
 
+  def show
+    @user = User.find_by(id: @contribution.user_id)
+  end
+
+  def new
+    @contribution = Contribution.new
+  end
+
+  def create
+    @contribution = Contribution.new(contribution_params)
+    if @contribution.save
+      redirect_to contributions_path
+    else
+      render 'new'
     end
+  end
 
-    def show
-        @contribution = Contribution.find(params[:id])
-        @user = User.find_by(id: @contribution.user_id)
+  def edit
+  end
+
+  def update
+    if @contribution.update(contribution_params)
+      redirect_to contributions_path
+    else
+      render 'edit'
     end
+  end
 
-    def new
-        @contribution = Contribution.new
-    end
+  def destroy
+    @contribution.destroy
+    redirect_to contributions_path
+  end
 
-    def create
-        @contribution = Contribution.new(contribution_params)
-        @user = current_user
-        @contribution.user_id = @user.id
-      if @contribution.save
-            redirect_to contributions_path
-      else
-            render 'new'
-      end
-    end
+private
+  def contribution_params
+    params.require(:contribution).permit(:title , :body , :option_id, :user_id)
+  end
 
-    def edit
-        @contribution = Contribution.find(params[:id])
-    end
+  def set_contribute
+    @contribution = Contribution.find(params[:id])
+  end
 
-    def update
-        @contribution = Contribution.find(params[:id])
-        if @contribution.update(contribution_params)
-            redirect_to contributions_path
-        else
-            render 'edit'
-        end
-    end
-
-    def destroy
-        @contribution = Contribution.find(params[:id])
-        @contribution.destroy
-        redirect_to contributions_path
-    end
-
-    private
-        def contribution_params
-            params.require(:contribution).permit(:title , :body , :option_id).merge(user_id: current_user.id)
-        end
 
 end
